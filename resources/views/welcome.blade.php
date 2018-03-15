6f1d8bfc762f23e110c491499c48d1e16a263901
andrej <pirog.andrej@gmail.com>
2018-03-15 08:52:35 +0200

    master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,7 +103,7 @@
                     <thead>
                     <tr>
                         <th>Имя</th>
-                        <th>Почта Email</th>
+                        <th>Почта Email master</th>
                         <th>Телефон</th>
                         <th>Город</th>
                     </tr>
